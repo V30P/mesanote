b9145a3d0fb124a0c79cc13c5a947fd5cc7a0d18
V30P <nhenning06@gmail.com>
2026-08-12 13:16:26 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,36 +2,43 @@
   description = "MesaNote dev environment";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs =
-    {
-      self,
-      flake-utils,
-      nixpkgs,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.python314
-            pkgs.poetry
-          ];
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-          # Install the core CLI and activate venv
-          shellHook = ''
-            cd core
-            poetry install -q
-            source "$(poetry env info --path)/bin/activate"
-            cd ..
-          '';
-        };
-      }
-    );
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.python314
+              pkgs.poetry
+            ];
+
+            # Install the core CLI and activate venv
+            shellHook = ''
+              cd core
+              poetry install -q
+              source "$(poetry env info --path)/bin/activate"
+              cd ..
+            '';
+          };
+        }
+      );
+    };
 }
